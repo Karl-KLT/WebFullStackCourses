b55e7ccfb487d9cbcbd22bc47316af7a7d0d6923
Karl-KLT <mo1670448@gmail.com>
2023-03-26 17:40:40 +0200

    fix text of comments

--- database/migrations/2023_03_05_062126_create_comments_table.php
+++ database/migrations/2023_03_05_062126_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('text');
+            $table->longText('text');
             $table->integer('blog_id');
             $table->integer('user_id');
             $table->timestamps();
